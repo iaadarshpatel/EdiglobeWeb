39fe7821cb93fa06adec12910b8da111da2fb8c9
iaadarshpatel <as7128@srmist.edu.in>
2024-01-26 17:41:32 +0530

    asas

--- src/Component/All-sub-pages/AllEmployee/Preloader.jsx
+++ src/Component/All-sub-pages/AllEmployee/Preloader.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import preloader from '../../../assets/loader2.mp4'
 
-
 const Preloader = () => {
   return (
     <>
-    <div className="d-flex justify-content-center align-items-center vh-100">
-      <video className="preloader-video" src={preloader} autoPlay loop muted />
+    <div className="d-flex justify-content-center bg-white align-items-center vh-100 overflow-hidden">
+      <video className="preloader-video"src={preloader} autoPlay loop muted />
     </div>
     </>
   )
