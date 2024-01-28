641b33d5ca5f4ecc8c665466b20da248c639b82b
iaadarshpatel <as7128@srmist.edu.in>
2024-01-28 19:40:00 +0530

    Updated

--- src/Component/All-sub-pages/AllEmployee/Preloader.jsx
+++ src/Component/All-sub-pages/AllEmployee/Preloader.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import preloader from '../../../assets/loader2.mp4'
 import './allemployee.css';
+import loader from '../../../assets/Logo3.gif'
 
 const Preloader = () => {
   return (
     <div className="preloader-container">
-      <video className="preloader-video" src={preloader} autoPlay loop muted />
+      <img className='preloader-video' src={loader} alt="pre-loader" />
     </div>
   );
 };
